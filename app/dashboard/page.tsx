dba83415c532f59341af0ba9b28af729c6c741cd
Sudhanshu <sudhanshusaudiyal15@gmail.com>
2024-10-14 23:18:38 +0530

    dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,42 @@
+"use client";
 
-export default function Dashboard() {
-  return(
-  <div>
-      hey there this is Dashboard
-  </div>
+import ChatsContacts from '@/components/ChatsContacts';
+import ChattingUser from '@/components/ChattingUser';
+import SearchUsername from '@/components/SearchUsername'
+import { User } from '@/lib/schema';
+import React, { useState } from 'react'
+import { SessionProvider } from "next-auth/react"
+
+const Dashboard = () => {
+  const [selectedUser, setSelectedUser] = useState<User | null>(null);
+  const [selectedRoom, setSelectedRoom] = useState<string>();
+
+  return (
+          <SessionProvider>
+
+    <div className="flex flex-col h-screen">
+      <header className="flex items-center justify-center p-4 border-b">
+        <SearchUsername setSelectedUser={setSelectedUser} setSelectedRoom={setSelectedRoom} />
+      </header>
+      
+      <main className="flex flex-1 overflow-hidden">
+        <aside className="w-1/3 border-r overflow-y-auto">
+          <ChatsContacts 
+            selectedUser={selectedUser} 
+            setSelectedUser={setSelectedUser} 
+            setSelectedRoom={setSelectedRoom}
+            />
+        </aside>
+        
+        <section className="flex-1 overflow-hidden">
+          <ChattingUser selectedUser={selectedUser}
+          selectedRoom={selectedRoom}
+          />
+        </section>
+      </main>
+    </div>
+          </SessionProvider>
   )
 }
+
+export default Dashboard
