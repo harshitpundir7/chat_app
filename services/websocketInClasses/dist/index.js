af0732a72d4473bda740a6e5b5f43440787799ca
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2025-03-07 17:47:05 +0530

    maded few changes

--- services/websocketInClasses/dist/index.js
+++ services/websocketInClasses/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const websocketClasses_1 = require("./websocketClasses");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.configDotenv)();
+const port = process.env.PORT;
+console.log(port);
 const server = new websocketClasses_1.websocketClasses(8080);
 server.start();
--- services/websocketInClasses/src/index.ts
+++ services/websocketInClasses/src/index.ts
@@ -1,4 +1,8 @@
 import { websocketClasses } from "./websocketClasses";
+import { configDotenv } from "dotenv"
 
+configDotenv()
+const port = process.env.PORT
+console.log(port)
 const server = new websocketClasses(8080);
 server.start();
